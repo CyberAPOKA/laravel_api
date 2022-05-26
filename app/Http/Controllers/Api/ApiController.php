2391c312475ab77ba507af92c2b0083fc49af19a
CyberAPOKA <oficialsteffens@hotmail.com>
2022-05-26 13:30:40 -0300

    create controoler, route and model for api

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 
@@ -13,7 +13,7 @@ class ApiController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json('ok');
     }
 
     /**
